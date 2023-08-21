6528d9f6a3a9884afda7df9f0cd33ba2cefd9ab8
kawin7538 <kawin7538@hotmail.com>
2023-08-21 16:03:39 +0700

    update laboratory skipping and others

--- simulation_module/hospital.py
+++ simulation_module/hospital.py
@@ -7,13 +7,15 @@ import simpy
 from .patient import Patient
 
 NUM_OPD_DEPARTMENT_COUNTER=15
+NUM_LABORATORY_DEPARTMENT_WORKSTATION=15
 
-AVG_OPD_USAGE=15
+AVG_OPD_USAGE=10
+AVG_LABORATORY_USAGE=120
 
 SIM_TIME=365*24*60
 START_DATETIME=datetime(year=2022,month=1,day=1)
 
-arrival_rate_df=pd.read_parquet("output/hourly_patient_arrival_rate.parquet")
+arrival_rate_df=pd.read_parquet("output/minutely_patient_arrival_rate_display_hourly.parquet")
 
 class Hospital:
     def __init__(self) -> None:
@@ -23,38 +25,97 @@ class Hospital:
         self.patient_counter=0
 
         self.opd_department=simpy.Resource(self.env,capacity=NUM_OPD_DEPARTMENT_COUNTER)
+        self.laboratory_department=simpy.Resource(self.env,capacity=NUM_LABORATORY_DEPARTMENT_WORKSTATION)
+
+        self.time_recorder=dict()
 
     def generate_patient_arrivals(self):
+
         while True:
+
+            if (START_DATETIME+timedelta(minutes=self.env.now)).hour<5 or (START_DATETIME+timedelta(minutes=self.env.now)).hour>15:
+                # print(f"Skipped at {(START_DATETIME+timedelta(minutes=self.env.now))}")
+                yield self.env.timeout(30)
+                continue;
+
             self.patient_counter+=1
             patient=Patient(self.patient_counter)
 
+            self.time_recorder[patient.patient_id]=dict()
+
             opd_process=self.env.process(self.attend_opd(patient=patient))
+            laboratory_process=self.env.process(self.attend_laboratory(patient=patient,opd_process=opd_process))
 
             if (START_DATETIME+timedelta(minutes=self.env.now)).weekday()>=5:
                 temp_interarrival_value=arrival_rate_df[(arrival_rate_df['InDateTime_onworkday']==False)&(arrival_rate_df['InDateTime_hour']==(START_DATETIME+timedelta(minutes=self.env.now)).hour)]['counter'].values[0]
             else:
                 temp_interarrival_value=arrival_rate_df[(arrival_rate_df['InDateTime_onworkday']==True)&(arrival_rate_df['InDateTime_hour']==(START_DATETIME+timedelta(minutes=self.env.now)).hour)]['counter'].values[0]
             
-            sampled_interarrival=random.expovariate(1.0/temp_interarrival_value)
+            sampled_interarrival=random.expovariate(temp_interarrival_value)
 
             yield self.env.timeout(sampled_interarrival)
 
     def attend_opd(self,patient:Patient):
         
-        print(f"Patient {patient.patient_id} started OPD queuing at {START_DATETIME+timedelta(minutes=self.env.now)}")
+        # print(f"Patient {patient.patient_id} started OPD queuing at {START_DATETIME+timedelta(minutes=self.env.now)}")
+        self.time_recorder[patient.patient_id]['datetime_opd_queuing']=START_DATETIME+timedelta(minutes=self.env.now)
+        self.time_recorder[patient.patient_id]['length_inqueue_opd']=len(self.opd_department.queue)
+
+        # for wating wait for doctor in OPD
+        while (START_DATETIME+timedelta(minutes=self.env.now)).hour<7:
+            # print(f"Skipped at {(START_DATETIME+timedelta(minutes=self.env.now))}")
+            yield self.env.timeout(30)
+            continue;
+        
+        # for lunch
+        while (START_DATETIME+timedelta(minutes=self.env.now)).hour==12:
+            # print(f"Skipped at {(START_DATETIME+timedelta(minutes=self.env.now))}")
+            yield self.env.timeout(15)
+            continue;
 
         with self.opd_department.request() as req:
+            
             yield req
 
-            print(f"Patient {patient.patient_id} started OPD at {START_DATETIME+timedelta(minutes=self.env.now)}")
+            # print(f"Patient {patient.patient_id} started OPD at {START_DATETIME+timedelta(minutes=self.env.now)}")
+            self.time_recorder[patient.patient_id]['datetime_opd']=START_DATETIME+timedelta(minutes=self.env.now)
 
             sampled_opd_usage=random.expovariate(1.0/AVG_OPD_USAGE)
 
             yield self.env.timeout(sampled_opd_usage)
 
-            print(f"Patient {patient.patient_id} finished OPD at {START_DATETIME+timedelta(minutes=self.env.now)}")
+            # print(f"Patient {patient.patient_id} finished OPD at {START_DATETIME+timedelta(minutes=self.env.now)}")
+            self.time_recorder[patient.patient_id]['datetime_opd_finished']=START_DATETIME+timedelta(minutes=self.env.now)
+
+    def attend_laboratory(self, patient:Patient, opd_process):
+        yield opd_process
+
+        if patient.specification['need_laboratory']==False:
+            # print(f"LAB Skipped for patient {patient.patient_id} at {(START_DATETIME+timedelta(minutes=self.env.now))}")
+            yield self.env.timeout(0)
+
+        else:
+
+            self.time_recorder[patient.patient_id]['datetime_laboratory_queuing']=START_DATETIME+timedelta(minutes=self.env.now)
+            self.time_recorder[patient.patient_id]['length_inqueue_laboratory']=len(self.laboratory_department.queue)
+
+            with self.laboratory_department.request() as req:
+                
+                yield req
+
+                # print(f"Patient {patient.patient_id} started LAB at {START_DATETIME+timedelta(minutes=self.env.now)}")
+                self.time_recorder[patient.patient_id]['datetime_laboratory']=START_DATETIME+timedelta(minutes=self.env.now)
+
+                sampled_laboratory_usage=random.expovariate(1.0/AVG_LABORATORY_USAGE)
+
+                yield self.env.timeout(sampled_laboratory_usage)
+
+                # print(f"Patient {patient.patient_id} finished LAB at {START_DATETIME+timedelta(minutes=self.env.now)}")
+                self.time_recorder[patient.patient_id]['datetime_laboratory_finished']=START_DATETIME+timedelta(minutes=self.env.now)
 
     def run(self):
         self.env.process(self.generate_patient_arrivals())
         self.env.run(until=SIM_TIME)
+
+    def get_time_recorder(self)->dict[(int,dict)]:
+        return self.time_recorder
--- simulation_module/patient.py
+++ simulation_module/patient.py
@@ -1,3 +1,34 @@
+import random
+import numpy as np
+import pandas as pd
+
+LIST_SPECIFICATION=[
+    'need_laboratory'
+]
+
+LIST_SPECIFICATION_IN_DF=[
+    'need_lab'
+]
+
+opd_final_df=pd.read_parquet("input_data/opd_final_df.parquet")
+
 class Patient:
     def __init__(self,patient_id):
         self.patient_id=patient_id
+        
+        self.random_patient=np.random.choice([True,False],p=[(471306-133074)/471306,133074/471306])
+
+        self.specification=dict()
+
+        self._define_specification()
+
+    def _define_specification(self):
+        if self.random_patient:
+            for idx in LIST_SPECIFICATION:
+                self.specification[idx]=False
+
+        else:
+            temp_specification=opd_final_df.sample(1)
+            for idx, col_df in zip(LIST_SPECIFICATION,LIST_SPECIFICATION_IN_DF):
+                self.specification[idx]=temp_specification[col_df].values[0]
+                # self.specification[idx]=True
